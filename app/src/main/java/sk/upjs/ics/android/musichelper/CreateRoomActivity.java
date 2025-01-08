6b932507d849d29b2b5f0c8eb271ac4e5a95d0f2
Vilo554321 <viliamdzurenda@gmail.com>
2025-01-08 10:22:53 +0100

    Zmena importu

--- app/src/main/java/sk/upjs/ics/android/musichelper/CreateRoomActivity.java
+++ app/src/main/java/sk/upjs/ics/android/musichelper/CreateRoomActivity.java
@@ -1,7 +1,6 @@
 package sk.upjs.ics.android.musichelper;
 
 import android.os.Bundle;
-import android.os.PersistableBundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
