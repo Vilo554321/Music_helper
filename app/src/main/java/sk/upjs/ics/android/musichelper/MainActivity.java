9be665c98ee7ad67c523febd895af614d1ba0e40
Vilo554321 <viliamdzurenda@gmail.com>
2025-01-19 20:41:09 +0100

    vymazanie waringov

--- app/src/main/java/sk/upjs/ics/android/musichelper/MainActivity.java
+++ app/src/main/java/sk/upjs/ics/android/musichelper/MainActivity.java
@@ -2,7 +2,6 @@ package sk.upjs.ics.android.musichelper;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -16,19 +15,9 @@ public class MainActivity extends AppCompatActivity {
         Button createRoomButton = findViewById(R.id.btnCreateRoom);
         Button joinRoomButton = findViewById(R.id.btnJoinRoom);
 
-        createRoomButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CreateRoomActivity.class));
-            }
-        });
+        createRoomButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, CreateRoomActivity.class)));
 
-        joinRoomButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, JoinRoomActivity.class));
-            }
-        });
+        joinRoomButton.setOnClickListener(v -> startActivity(new Intent(MainActivity.this, JoinRoomActivity.class)));
     }
 }
 
